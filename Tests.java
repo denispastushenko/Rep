92397dec6e4204d27e86182f1f850a6dbfa39b44
Padavan1 <dpastushenko1996@mail.com>
2016-05-26 17:37:06 -0700

    Gir

--- Tests.java
+++ Tests.java
@@ -3,34 +3,32 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Random;
 
-/**
- * Created by Billy on 5/24/2016.
- */
+
+
 public class Tests {
-    private static void createFolder(int a, int b, int c) {
-        String homeDirectory = "Z:\\My_test\\Folder";
+    public static void main(String[] args) {
         String homeSubDirectory = "SubFolder";
         String filename = "File";
         boolean bool = false;
         File folder = null;
         Random random = new Random();
-        for (int y = 0; y < a; y++) {
+        for ( int y = 0; y < Integer.parseInt(args[0]); y++) {
             try {
-                folder = new File(homeDirectory.concat(String.valueOf(y + 1)));
-                bool = folder.mkdirs();
+                folder = new File(args[3].concat(String.valueOf(y + 1)));
+                bool = folder.mkdir();
                 System.out.println(bool);
             } catch (Exception e2) {
                 e2.printStackTrace();
             }
-            System.out.println(y + " " + homeDirectory);
-            for (int i = 0; i < random.nextInt(b); i++) {
+            System.out.println(y + " " + args[3]);
+            for (int i = 0; i < random.nextInt(Integer.parseInt(args[1])); i++) {
                 File subfolder = new File(folder, homeSubDirectory.concat(String.valueOf(i + 1)));
-                subfolder.mkdirs();
-                for (int i3 = 0; i3 < random.nextInt(c) + 1; i3++) {
+                subfolder.mkdir();
+                for (int i3 = 0; i3 < random.nextInt(Integer.parseInt(args[2])) + 1; i3++) {
                     try {
                         FileWriter writer = new FileWriter(new File(subfolder, filename.concat(String.valueOf(i3++)).concat(".txt")));
                         String string = "";
-                        for (int i4 = 65, i5 = 0; i5 < random.nextInt(a) * random.nextInt(b) * c + 1024; i5++) {
+                        for (int i4 = 65, i5 = 0; i5 < random.nextInt(Integer.parseInt(args[0])) * random.nextInt(Integer.parseInt(args[1])) * Integer.parseInt(args[2]) + 1024; i5++) {
                             string = string.concat(String.valueOf((char) i4++));
                         }
                         writer.write(string);
@@ -44,8 +42,4 @@ public class Tests {
         }
     }
 
-    public static void main(String[] args) {
-        createFolder(4, 7, 10);
-
-    }
 }
