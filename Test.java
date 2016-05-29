8b80240e51b6a4f6314d814ecec37e723265d590
Padavan1 <dpastushenko1996@mail.com>
2016-05-29 14:53:44 +0300

    dfgd

---
+++ Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Denys on 5/19/2016.
+ */
+public class Test {
+    public static void main(String[] args) {
+    int i =1,x,y;
+        x = ++i + i++;
+        y = i++ + ++i;
+        System.out.println( "Я нихуя не понимаю,почему так!!?? Прочитал дохера статей и всеровно не понимаю! "+ x +  " "+ y );
+
+    }
+}
