48eb3f17b41dfd2534de2cc5cb9a727ea5890177
Padavan1 <dpastushenko1996@mail.com>
2016-06-07 23:29:42 +0300

    downloads1

--- SecondTest/gerImage/src/test/java/ImageDownloadTest.java
+++ SecondTest/gerImage/src/test/java/ImageDownloadTest.java
@@ -1,21 +1,19 @@
+import com.ImageDownloader;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.io.File;
 import java.io.IOException;
+import static com.ImageDownloader.*;
 
-/**
- * Created by Denys on 6/5/2016.
- */
 public class ImageDownloadTest {
-    File file = null;
+    private File file = null;
 
     @Test
-    public void imaageDownloadTest() throws IOException {
+    public void imageDownloadTest() throws IOException {
         ImageDownloader iD = new ImageDownloader();
-        file = iD.saveImage(iD.I_URL, iD.FILE_WAY);
-        Assert.assertTrue(iD.compareFiles(file, iD.IMAGE));
+        file = iD.saveImage(I_URL, FILE_WAY);
+        Assert.assertTrue(iD.compareFiles(file, IMAGE));
     }
 
     @After
